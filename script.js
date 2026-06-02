51913ce22ddb93b17484f6778da4b295f94338b9
giftmshengu250-pixel <your.github.giftmshengu250@gmail.com>
2026-06-02 13:07:22 +0200

    Initial commit for new project

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll when clicking navbar links
+document.querySelectorAll('a[href^="#"]').forEach(function(link) {
+  link.addEventListener('click', function(e) {
+    var target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
